0f9bc0fda0234ccfe259c5e15480c5b2e7efb05d
xiejiaqi.jackey <xiejiaqi.jackey@bytedance.com>
2026-06-21 21:16:23 +0800

    feat: update

--- core/scripts/carla/policies/smpc_agent.py
+++ core/scripts/carla/policies/smpc_agent.py
@@ -54,7 +54,7 @@ class SMPCAgent(object):
                  yield_stop_decel=-5.0,
                  yield_conflict_radius=4.0,
                  yield_stop_buffer_distance=6.25,
-                 yield_brake_distance_margin=3.0,
+                 yield_brake_distance_margin=3.5,
                  yield_wait_steer_lookahead_distance=6.0,
                  yield_wait_steer_gain=1.0,
                  yield_ttc_margin=0.8,
